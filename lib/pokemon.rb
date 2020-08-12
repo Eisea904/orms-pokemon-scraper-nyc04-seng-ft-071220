566c1a5e692377ed51e2e8f3b6f12c5d05b6c1d5
Alex E(She/Her) <eisea904@newschool.edu>
2020-08-12 12:01:46 -0400

    7 examples, 3 failures

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :database 
+    
+    def initialize(id=nil, name, type, database)
+        @id = id
+        @name = name
+        @type = type
+        @db = database 
+    end 
+
+    def self.create_table
+        sql =  <<-SQL 
+        CREATE TABLE IF NOT EXITS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
+        SQL
+        DB[:conn].execute(sql)
+    end 
+    def self.save(id, name, type)
+        sql = <<-SQL
+        INSERT INTO pokemon (id, name, type)
+        VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.type)
+    end 
+
+
+
+    
+
 end
